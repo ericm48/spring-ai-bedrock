e9c3ddc3c35af97dd9fe493ce3b5ce941830e5fa
badraoul <watkinss@vmware.com>
2024-04-05 09:18:07 -0700

    removed references to specific BedrockCohereChatClient

--- src/main/java/dev/steve/dadjokes/ChatController.java
+++ src/main/java/dev/steve/dadjokes/ChatController.java
@@ -1,13 +1,30 @@
 package dev.steve.dadjokes;
 
-import org.springframework.ai.BedrockCohereChatClient;
-import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatClient;   //**for some reason this bean is not loading**
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// @RestController
+
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    
+    @GetMapping("/dad-jokes")
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a dad joke") String message) {
+        return chatClient.call(message);
+    }
+} 
+
+
+// *** below was copied from SpringAI documentation specifically for Cohere ***
+//@RestController
 // public class ChatController {
 
 //     private final BedrockCohereChatClient chatClient;
@@ -29,23 +46,6 @@ import org.springframework.web.bind.annotation.RestController;
 //     }
 // }
 
-
-
-
-public class ChatController {
-
-    private final ChatClient chatClient;
-
-    public ChatController(ChatClient chatClient) {
-        this.chatClient = chatClient;
-    }
-
-    
-    @GetMapping("/dad-jokes")
-    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a dad joke") String message) {
-        return chatClient.call(message);
-    }
-} 
 
 
 
